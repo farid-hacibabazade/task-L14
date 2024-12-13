d57902d929cf9e3bbbc33f85294f01ee02e21c9e
farid-hacibabazade <faridhaci0@gmail.com>
2024-12-13 22:01:01 +0400

    initial commit

---
+++ src/main/java/az/ingress/taskl14/TaskL14Application.java
@@ -0,0 +1,13 @@
+package az.ingress.taskl14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskL14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskL14Application.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingress/taskl14/TaskL14ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.taskl14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskL14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
